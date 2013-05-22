0be2d78da350ff301a3ac85264a46c25dfd59d95
jameshines10 <jhines@pykl.com>
2013-05-21 23:45:56 -0400

    Added a filter that displays even or odd numbers depending on which button you press

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,40 @@
 'use strict';
 
 angular.module('angularFiltersApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', ['$filter', '$scope', function ($filter, $scope) {
+    $scope.filtered = [];
+    $scope.numbers = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
     ];
-  });
+
+    $scope.filtered = $scope.numbers;
+
+    $scope.filterNumbers = function(parity){
+        var filtered = [];
+
+        if(parity != 'all'){
+            filtered = $filter('filter')($scope.numbers, function(number){
+                if(parity == 'even' && parseInt(number) % 2 == 0){
+                    return number;
+                }
+
+                if(parity == 'odd' && parseInt(number) % 2 != 0){
+                    return number;
+                }
+            });
+
+            $scope.filtered = filtered;
+        }else{
+            $scope.filtered = $scope.numbers;
+        }
+    }
+
+  }]);
