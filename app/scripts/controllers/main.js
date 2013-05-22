21d16a671a1be80660da3d41479ff46787f934a4
jameshines10 <jhines@pykl.com>
2013-05-21 23:03:41 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularFiltersApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
